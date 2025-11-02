3c0366587943dfd400a1e2e831514e23a9fe4ffa
piyushtamboli734 <digitalpiyush734@gmail.com>
2025-11-02 17:24:50 +0530

    c1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,23 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
